c575731c1923f24554c93acc5ebbeb6bb9d4148f
Matthias <beef@0xdead.de>
2017-12-07 18:32:19 +0100

    Increase P-value to make sharper curves

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,7 @@ int main()
 {
   uWS::Hub h;
 
-  PID pid(0.1, 0.001, 0.7);
+  PID pid(0.1, 0.001, 3.0);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
